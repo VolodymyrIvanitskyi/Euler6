7c340d26403418adbdb4f7ab7609085c9c8a2993
Vova_Ivanitskyi <vovanhuk2003@gmail.com>
2020-06-23 17:30:19 +0300

    Добавьте файлы проекта.

---
+++ Euler6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Euler6
+{
+    class Square_Sum
+    {
+        static void Main(string[] args)
+        {
+            long result = SquareSum(100) - TheSumOfSquares(100);
+            Console.WriteLine($"result = {result}");
+        }
+
+        static long TheSumOfSquares(int number) // сума квадратів (number - кількість чисел доки проводити розрахунки)
+        {
+            long Sum = 0;
+
+            for (int i = 1; i <= number; i++)
+                Sum += i*i;
+            return Sum;
+        }
+
+        static long SquareSum(int number) // квадрат суми (number - кількість чисел доки проводити розрахунки)
+        {
+            long Sum = 0;
+            for (int i = 1; i <= number; i++) //Пошук суми
+                Sum += i;
+            return Sum*Sum; // Повертаємо квадрат суми
+        }
+    }
+}
